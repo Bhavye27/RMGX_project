cadc133a1f4043d0ab5328c619b197bc23e92ce8
Bhavye Budhiraja <bhavyebudhiraja27@gmail.com>
2018-08-24 01:45:39 +0530

    click-to-change-images

--- app.js
+++ app.js
@@ -24,3 +24,20 @@ function image5()
     document.getElementById("back").style.background="url(images/main5.jpg)";
     document.getElementById("back").style.backgroundSize= "100% 100%";
 }
+
+function leftImage()
+{
+    //console.log("abc");
+    document.getElementById("first-room").src = "images/room1.jpg";
+    document.getElementById("second-room").src = "images/room2.jpg";
+    document.getElementById("third-room").src = "images/room3.jpg";
+    document.getElementById("fourth-room").src = "images/room4.jpg";
+}
+
+function rightImage()
+{
+    document.getElementById("first-room").src = "images/room5.jpg";
+    document.getElementById("second-room").src = "images/room6.jpg";
+    document.getElementById("third-room").src = "images/room7.jpg";
+    document.getElementById("fourth-room").src = "images/room8.jpg";
+}
