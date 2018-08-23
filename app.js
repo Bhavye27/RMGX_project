e34002782905e37a75c2975f5a9e263713128cc2
Bhavye Budhiraja <bhavyebudhiraja27@gmail.com>
2018-08-23 13:15:22 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function image1()
+{
+    document.getElementById("back").style.background= "url(images/main1.jpg)";
+    document.getElementById("back").style.backgroundSize= "1370px 500px";
+    
+}
+function image2()
+{
+    document.getElementById("back").style.background="url(images/main2.jpeg)";
+    document.getElementById("back").style.backgroundSize= "1370px 500px";
+}
+function image3()
+{
+    document.getElementById("back").style.background="url(images/main3.jpeg)";
+    document.getElementById("back").style.backgroundSize= "1370px 500px";
+}
+function image4()
+{
+    document.getElementById("back").style.background="url(images/main4.jpeg)";
+    document.getElementById("back").style.backgroundSize= "1370px 500px";
+}
+function image5()
+{
+    document.getElementById("back").style.background="url(images/main5.jpg)";
+    document.getElementById("back").style.backgroundSize= "1370px 500px";
+}
