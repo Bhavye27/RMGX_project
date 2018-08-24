901d7aeba4bf5633d21491bbdc0ee1acafc636ce
Bhavye Budhiraja <bhavyebudhiraja27@gmail.com>
2018-08-25 00:05:20 +0530

    changed images

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function image1()
 {
-    document.getElementById("back").style.background= "url(images/main1.jpg)";
+    document.getElementById("back").style.background= "url(images/main4.jpeg)";
     document.getElementById("back").style.backgroundSize= "100% 100%";
     
 }
@@ -16,7 +16,7 @@ function image3()
 }
 function image4()
 {
-    document.getElementById("back").style.background="url(images/main4.jpeg)";
+    document.getElementById("back").style.background="url(images/main1.jpg)";
     document.getElementById("back").style.backgroundSize= "100% 100%";
 }
 function image5()
