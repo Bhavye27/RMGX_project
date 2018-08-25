b3f39c22ceae56c280ff022d16002bec24eb4da4
Bhavye Budhiraja <bhavyebudhiraja27@gmail.com>
2018-08-25 13:14:47 +0530

    added Carousel

--- app.js
+++ app.js
@@ -1,33 +1,6 @@
-function image1()
-{
-    document.getElementById("back").style.background= "url(images/main4.jpeg)";
-    document.getElementById("back").style.backgroundSize= "100% 100%";
-    
-}
-function image2()
-{
-    document.getElementById("back").style.background="url(images/main2.jpeg)";
-    document.getElementById("back").style.backgroundSize= "100% 100%";
-}
-function image3()
-{
-    document.getElementById("back").style.background="url(images/main3.jpeg)";
-    document.getElementById("back").style.backgroundSize= "100% 100%";
-}
-function image4()
-{
-    document.getElementById("back").style.background="url(images/main1.jpg)";
-    document.getElementById("back").style.backgroundSize= "100% 100%";
-}
-function image5()
-{
-    document.getElementById("back").style.background="url(images/main5.jpg)";
-    document.getElementById("back").style.backgroundSize= "100% 100%";
-}
 
 function leftImage()
 {
-    //console.log("abc");
     document.getElementById("first-room").src = "images/room1.jpg";
     document.getElementById("second-room").src = "images/room2.jpg";
     document.getElementById("third-room").src = "images/room3.jpg";
@@ -41,6 +14,7 @@ function rightImage()
     document.getElementById("third-room").src = "images/room7.jpg";
     document.getElementById("fourth-room").src = "images/room8.jpg";
 }
+
 $('#datepicker1').datepicker({
             uiLibrary: 'bootstrap4'
         });
